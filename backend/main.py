4b832068b3b2fe9fb2e5ef6f60ba590860ae2848
ASPactores <anakin.pactores@gmail.com>
2025-02-17 05:22:12 +0800

    feat: add CORS middleware to enable cross-origin requests

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from mangum import Mangum
 from api import auth, vouchers
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Include routes
 app.include_router(auth.router, prefix="/auth", tags=["Auth"])
