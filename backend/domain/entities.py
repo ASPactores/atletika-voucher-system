93370a01dc20a45546f698c865151c7e769709c2
ASPactores <anakin.pactores@gmail.com>
2025-02-17 05:24:01 +0800

    feat: enhance Voucher class to include expiry date and handle expired vouchers

--- backend/domain/entities.py
+++ backend/domain/entities.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
+from datetime import datetime
 from constants.enums import VoucherStatus
+from fastapi import HTTPException
 
 
 @dataclass
@@ -7,10 +9,15 @@ class Voucher:
     voucher_id: str
     first_name: str
     last_name: str
+    expiry_date: str
     percentage: str
     status: str = VoucherStatus.UNUSED.value
 
     def mark_as_used(self):
         if self.status == VoucherStatus.USED.value:
-            raise ValueError("Voucher has already been used.")
+            raise HTTPException(
+                status_code=400, detail="Voucher has already been claimed."
+            )
+        if datetime.now() > datetime.fromisoformat(self.expiry_date):
+            raise HTTPException(status_code=400, detail="Voucher has expired.")
         self.status = VoucherStatus.USED.value
