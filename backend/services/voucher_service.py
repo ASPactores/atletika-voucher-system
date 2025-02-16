95ecdbe1f820d03500e1e65bb1afc4710050cbf2
ASPactores <anakin.pactores@gmail.com>
2025-02-17 05:28:44 +0800

    feat: enhance voucher service to include expiry date in voucher creation and retrieval

--- backend/services/voucher_service.py
+++ backend/services/voucher_service.py
@@ -1,19 +1,37 @@
 import uuid
-from domain.models import VoucherList
+from io import BytesIO
+from domain.models import VoucherResponse
 from domain.entities import Voucher
 from infrastructure.dynamodb import table
 from utils.qr_generator import generate_qr_code
+from utils.send_email import send_email
 from constants.enums import VoucherStatus
 
 
-def create_voucher(first_name: str, last_name: str, percentage: str) -> Voucher:
+# TODO: Remove this import
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def create_voucher(
+    first_name: str,
+    last_name: str,
+    expiry_date: str,
+    percentage: str,
+) -> BytesIO:
     unique_id = str(uuid.uuid4())
-    image = generate_qr_code(unique_id)
+    image = generate_qr_code(unique_id, expiry_date)
+
+    # TODO: Remove this line. This is just for testing purposes.
+    send_email(os.getenv("EMAIL_ADDRESS"), image)
 
     voucher = {
         "voucher-id": unique_id,
         "first-name": first_name,
         "last-name": last_name,
+        "expiry-date": expiry_date,
         "percentage": percentage,
         "status": VoucherStatus.UNUSED.value,
     }
@@ -33,6 +51,7 @@ def claim_voucher(voucher_id: str):
         voucher_id=voucher_data["voucher-id"],
         first_name=voucher_data["first-name"],
         last_name=voucher_data["last-name"],
+        expiry_date=voucher_data["expiry-date"],
         percentage=voucher_data["percentage"],
         status=voucher_data["status"],
     )
@@ -43,6 +62,25 @@ def claim_voucher(voucher_id: str):
         UpdateExpression="SET #status = :new_status",
         ExpressionAttributeNames={"#status": "status"},
         ExpressionAttributeValues={":new_status": VoucherStatus.USED.value},
+    )
+
+    return voucher
+
+
+def get_voucher(voucher_id: str) -> Voucher:
+    response = table.get_item(Key={"voucher-id": voucher_id})
+    voucher_data = response.get("Item")
+
+    if not voucher_data:
+        raise ValueError("Voucher not found.")
+
+    voucher = VoucherResponse(
+        voucher_id=voucher_data["voucher-id"],
+        first_name=voucher_data["first-name"],
+        last_name=voucher_data["last-name"],
+        expiry_date=voucher_data["expiry-date"],
+        percentage=voucher_data["percentage"],
+        status=voucher_data["status"],
     )
 
     return voucher
